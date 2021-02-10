0524ca6b5c0750e75af09e2d0fe095dc03115fba
dandeduck <danielmkhlv@gmail.com>
2021-02-11 01:26:57 +0200

    strapped search and some changes to package track

--- src/packages/views.py
+++ src/packages/views.py
@@ -15,27 +15,35 @@ def partner_search(request, partner):
     number_query = request.GET.get('number')
 
     packages = Package.objects.filter(order__partner__name=partner.name)
+    filtered_packages = Package.objects.none()
 
     if name_query:
         packages = packages.filter(full_name__icontains=name_query)
+        filtered_packages = packages
     if number_query:
         packages = packages.filter(phone_number__icontains=number_query)
+        filtered_packages = packages
     if street_name_query:
         packages = packages.filter(destination__street_name__icontains=street_name_query)
+        filtered_packages = packages
     if street_number_query:
         packages = packages.filter(destination__street_number__contains=street_number_query)
+        filtered_packages = packages
     if city_query:
         packages = packages.filter(destination__city__name__icontains=city_query)
+        filtered_packages = packages
 
     orders = []
     related_packages = []
-    for package in packages.order_by('-order__collection_date'):
+    for package in filtered_packages.order_by('-order__collection_date'):
         order = package.order
         if order not in orders:
             orders.append(order)
-            related_packages.append(package)
+            related_packages.append([package])
+        else:
+            related_packages[orders.index(order)].append(package)
 
-    order_packages = [(orders[i], packages[i]) for i in range(0, len(orders))]
+    order_packages = [(orders[i], related_packages[i]) for i in range(0, len(orders))]
     context = {
         'partner': partner,
         'cities': City.objects.all(),
