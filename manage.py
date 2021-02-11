8b578ff81bc77092d935808a3d39ad85674c6282
dandeduck <danielmkhlv@gmail.com>
2021-02-11 21:04:57 +0200

    fixed structure

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'package-tracking.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
