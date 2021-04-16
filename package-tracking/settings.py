916cb136ee799a375efaaff0720f9726b4cad4de
dandeduck <danielmkhlv@gmail.com>
2021-04-16 14:29:27 +0300

    -_-

--- package-tracking/settings.py
+++ package-tracking/settings.py
@@ -124,16 +124,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
 STATIC_URL = '/static/'
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-# DataFlair
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'testingmonke@gmail.com'
-EMAIL_HOST_PASSWORD = '23uD8KSFXdanMfn'
